74f7badba78c7e91700ddf07e2564885aec9981d
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-19 19:41:23 +0000

    Give a unique key to every props

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,25 +1,35 @@
 import React from "react";
 
 const Movies = (props) => {
-  if (Array.isArray(props.movies) && (props.movies.length > 0)) {
-	//Don't forget to remove the items without an image
-	return (
-		<>
-		  {props.movies.map((movie, i) => (
-			<div className="d-flex flex-column justify-content-start m-3 auto-w image-container">
-			  <div className="card-container d-flex flex-column">
-				<img src={movie.Poster} alt="movie"></img>
-			  </div>
-			  <div
-				onClick={() => props.keepFavouritesClick(movie)}
-				className="img-overlay d-flex align-items-center justify-content-center"
-			  >
-			  </div>
-			  <div className="movie-title mt-4 mb-4 text-center">{movie.Title}</div>
-			</div>
-		  ))}
-		</>
-	  );
+  const Favourite = props.favourite;
+  if (Array.isArray(props.movies) && props.movies.length > 0) {
+    //Don't forget to remove the items without an image
+    return (
+      <>
+        {props.movies.map((movie, i) => (
+          <div key={i} className="d-flex flex-column justify-content-start m-3 auto-w image-container">
+            <div
+			className="card-container d-flex flex-column">
+              <img
+                src={movie.Poster}
+				onClick={() => props.keepWatchedClick(movie)}
+                alt="movie"
+              ></img>
+            </div>
+            <div
+              onClick={() => props.keepFavouritesClick(movie)}
+              className="img-overlay movies-overlay d-flex align-items-center justify-content-center"
+            >
+              <Favourite />
+            </div>
+
+            <div className="movie-title mt-4 mb-4 text-center">
+              {movie.Title}
+            </div>
+          </div>
+        ))}
+      </>
+    );
   }
 };
 
