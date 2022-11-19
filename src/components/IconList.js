8b18d40d1e5a957a11eb558d41be5df5065e24b0
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-19 19:41:03 +0000

    Add modal to watched movies

--- src/components/IconList.js
+++ src/components/IconList.js
@@ -2,45 +2,49 @@ import React from "react";
 import Movies from "./Movies";
 
 const Icons = () => {
-	return (
-		<div className="icons d-flex flex-row align-items-center">
-			<div
-				className="icon-favourites">
-				<svg
- 					onClick={() => {
-						document.getElementById('myFavouritesContent').focus()
-					}}
-					data-bs-toggle="modal"
-					data-bs-target="#myFavourites"
-					xmlns="http://www.w3.org/2000/svg"
-					width="18"
-					height="18"
-					fill="currentColor"
-					className="bi bi-heart-fill m-3"
-					viewBox="0 0 16 16"
-				>
-					<path
-						fillRule="evenodd"
-						d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-					/>
-				</svg>
-			</div>
-			|
-			<div className="icon-watched">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="18"
-					height="18"
-					fill="currentColor"
-					className="bi bi-eye m-3"
-					viewBox="0 0 16 16"
-				>
-					<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-					<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-				</svg>
-			</div>
-		</div>
-	);
+  return (
+    <div className="icons d-flex flex-row align-items-center">
+      <div className="icon-favourites">
+        <svg
+          onClick={() => {
+            document.getElementById("myFavouritesContent").focus();
+          }}
+          data-bs-toggle="modal"
+          data-bs-target="#myFavourites"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="currentColor"
+          className="bi bi-heart-fill m-3"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+          />
+        </svg>
+      </div>
+      |
+      <div className="icon-watched">
+        <svg
+          onClick={() => {
+            document.getElementById("myWatchedContents").focus();
+          }}
+          data-bs-toggle="modal"
+          data-bs-target="#myWatchedOnes"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="currentColor"
+          className="bi bi-eye m-3"
+          viewBox="0 0 16 16"
+        >
+          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Icons;
