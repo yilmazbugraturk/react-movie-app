99132f148cf7975fde05ad046f14ace06e46e78e
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-19 12:59:13 +0000

    Don't show any item when search is empty

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,19 @@ const SearchBar = (props) => {
   return (
     <div className="col col-sm-4">
       <input
+        id="searchBarbyName"
         className="form-control"
         value={props.value}
         onChange={(e) => {
-          props.setSearchValue(e.target.value);
+          if (e.target.value === "") {
+            const search_results =
+              document.getElementsByClassName("image-container");
+            for(let i=0;i <= search_results.length;++i){
+              search_results[i].innerHTML = "";
+            }
+          } else {
+            props.setSearchValue(e.target.value);
+          }
         }}
         placeholder="Search by Name..."
       ></input>
