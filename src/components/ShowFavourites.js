3ec28e6297a3dddcbe57c1cf8710e9d4b6bd4807
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-19 19:42:56 +0000

    Give classes to favourites parts for CSS purposes

--- src/components/ShowFavourites.js
+++ src/components/ShowFavourites.js
@@ -1,38 +1,51 @@
 import React from "react";
-import Movies from "./Movies";
 
 const Favourites = (props) => {
-	const Favourite = props.favourite;
+  const Favourite = props.favourite;
   return (
     <>
-	<div className="modal fade" id="myFavourites" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	<div className="modal-dialog modal-dialog-scrollable">
-		<div className="modal-content">
-		<div className="modal-header">
-			<h1 className="modal-title fs-5 favourites-title" >Favourites</h1>
-		</div>
-		<div className="modal-body">
-			{props.movies.map((movie, i) => (
-				<div className="d-flex flex-column justify-content-start m-3 auto-w image-container">
-				<div className="card-container d-flex flex-column">
-					<img src={movie.Poster} alt="movie"></img>
-				</div>
-				<div
-					onClick={() => props.keepFavouritesClick(movie)}
-					className="img-overlay d-flex align-items-center justify-content-center"
-				>
-					<Favourite />
-				</div>
-				<div className="movie-title mt-4 mb-4 text-center">{movie.Title}</div>
-				</div>
-			))}
-		</div>
-		<div className="modal-footer">
-			<button type="button" className="btn btn-dark" data-bs-dismiss="modal">Close</button>
-		</div>
-		</div>
-	</div>
-	</div>
+      <div
+        className="modal fade"
+        id="myFavourites"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-scrollable favourites-container">
+          <div className="modal-content ">
+            <div className="modal-header favourites-containerpart ml-3">
+              <h1 className="modal-title fs-5 favourites-title">Favourites</h1>
+            </div>
+            <div className="modal-body d-flex flex-row flex-wrap justify-content-between favourites-containerpart">
+              {props.movies.map((movie, i) => (
+                <div key={i} className="d-flex flex-column justify-content-start m-3 auto-w image-container">
+                  <div className="card-container d-flex flex-column">
+                    <img src={movie.Poster} alt="movie"></img>
+                  </div>
+                  <div
+                    onClick={() => props.keepFavouritesClick(movie)}
+                    className="img-overlay d-flex align-items-center justify-content-center"
+                  >
+                    <Favourite />
+                  </div>
+                  <div className="movie-title mt-4 mb-4 text-center">
+                    {movie.Title}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="modal-footer favourites-containerpart">
+              <button
+                type="button"
+                className="btn btn-dark favourites-close-button"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
