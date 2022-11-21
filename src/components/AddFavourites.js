8e65ad2da8ee49d6a160bb7a1b96e9a72639309b
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-21 12:31:53 +0000

    Change color of the add to favourites icon

--- src/components/AddFavourites.js
+++ src/components/AddFavourites.js
@@ -10,7 +10,7 @@ const AddFavourites = () => {
         xmlns="http://www.w3.org/2000/svg"
         width="16"
         height="16"
-        fill="red"
+        fill="#dc3545"
         className="bi bi-heart-fill"
         viewBox="0 0 16 16"
       >
