3971cf5d7f08f65fe1e9c95b6bfa5c887f0d3087
Yılmaz Türk <yturkk.04@gmail.com>
2022-11-21 12:32:38 +0000

    Update the component to show  full details of movies

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const MovieDetails = (props) => {
   return (
     <>
@@ -10,27 +11,34 @@ const MovieDetails = (props) => {
       aria-labelledby="MovieDetailsModalLabel"
       aria-hidden="true"
     >
-      <div className="modal-dialog modal-dialog-scrollable favourites-container">
+      <div className="modal-dialog modal-dialog-scrollable movie-details-container">
         <div className="modal-content ">
-          <div className="modal-header favourites-containerpart ml-3">
-            <h1 className="modal-title fs-5 favourites-title">
+          <div className="modal-header movie-details-containerpart ml-3">
+            <h1 className="modal-title fs-5 movie-details-title">
               {props.details.Title}
             </h1>
           </div>
-          <div className="modal-body d-flex flex-row flex-wrap justify-content-between favourites-containerpart">
+          <div className="modal-body d-flex flex-row flex-wrap justify-content-around movie-details-containerpart">
            <img
             className="modal-poster"
             alt="modal-poster"
             src={props.details.Poster}
            />
-           <div>{props.details.Year}</div>
-           <div>{props.details.Actors}</div>
-           <div>{props.details.Plot}</div>
+           <div className="details d-flex flex-column align-items-start justify-content-around">
+            <div><strong>Year: </strong>{props.details.Year}</div>
+            <div><strong>Date of release: </strong>{props.details.Released}</div>
+            <div><strong>Duration: </strong>{props.details.Runtime}</div>
+            <div><strong>Actors: </strong>{props.details.Actors}</div>
+            <div><strong>Genre: </strong>{props.details.Genre}</div>
+            <div><strong>Language(s): </strong>{props.details.Language}</div>
+            <div><strong>IMDB Ratings: </strong>{props.details.imdbRating} / 10</div>
+            <div><strong>Brief Description: </strong>{props.details.Plot}</div>
+           </div>
           </div>
-          <div className="modal-footer favourites-containerpart">
+          <div className="modal-footer movie-details-containerpart">
             <button
               type="button"
-              className="btn btn-dark favourites-close-button"
+              className="btn btn-dark movie-details-close-button"
               data-bs-dismiss="modal"
             >
               Close
